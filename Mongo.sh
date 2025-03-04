d2d1c9359b436717b3f6080d091e333726e38170
Tharun <tharunchowdary982@gmail.com>
2025-03-05 00:55:30 +0530

    roboshop code

--- Mongo.sh
+++ Mongo.sh
@@ -2,6 +2,6 @@ cp Mongo.repo /etc/yum.repos.d/Mongo.repo
 dnf install mongodb-org -y
 
 sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
-systemctl enable mongodb
-systemctl start mongodb
-systemctl restart mongodb
+systemctl enable mongod
+systemctl start mongod
+systemctl restart mongod
